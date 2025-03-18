fadfed773b978d2cb6627f27877e73ec0da4ceb0
rmirko00 <rmirko00@fesb.hr>
2025-03-18 20:05:06 +0100

    feat: dodana nova metoda

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ def nova_funkcija():
     return "Ovo je nova funkcija."
 
 def nova_metoda(self):
-    return "Ovo je nova metoda"
+    return "Ovo je nova metoda 2"
 
 # Ovo je moj komentar - Ema Medic
